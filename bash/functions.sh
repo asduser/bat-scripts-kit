721db9d9a6233a0395c00d346040b2ab17ae4ea7
developer <mailerboss00@mail.com>
2019-08-09 12:23:34 +0300

    Add bash scripts directory

---
+++ bash/functions.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# just show the message
+greeting() {
+    echo "Hello from bash!"
+}
+
+# ask username and password, -p (prompt), -sp (silent prompt)
+ask_credentials() {
+    read -p "Username: " username
+    read -sp "Password: " password
+    # echo "Username is: $username, password is: $password"
+    echo "Thank you, $username! We remember your credentials."
+}
+
+# two ways of command execution
+execute_commands() {
+    result1=`pwd`
+    echo "result1: $result1"
+
+    a=1
+    b=2
+    result2=$(($a + $b))
+    echo "result2: $result2"
+}
+
+# ask enter username and find value in users.txt
+find_user() {
+    read -p "Find user by name: " user
+    if grep $user ./files/users.txt
+    then
+    echo "User exists"
+    else
+    echo "User not found"
+    fi
+}
+
+# sort lines and write result into another file
+sort_users() {
+    sort ./files/users.txt > ./files/users_with_sorting.txt
+}
+
+# sort lines by column and write result into another file, -r (reverse), -n (numeric), -k (column number)
+sort_users_by_column() {
+    sort -k 2nr ./files/users_column.txt > ./files/users_column_with_sorting.txt
+}
+
+sort_users_by_column
+sleep 1s
---
+++ bash/read.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Enter name"
+
+read name
+
+echo "Your name is: " $name
+
+sleep 1s
